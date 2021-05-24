c29dc9d6dc02d4dc6d353aa8d949cc9b1459bcb1
John James Gutib <jjgutib@gmail.com>
2021-05-24 17:42:33 +0800

    Completed basic game flow and logic

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+    public static GameState state = GameState.StartMenu;
+
+    [SerializeField] GameObject startMenu;
+
+    public enum GameState {
+        StartMenu,
+        Running,
+        KickingBall,
+        BallKicked,
+        FinishScreen
+    }
+
+    void Awake() {
+        if (Instance != null)
+            Debug.LogError("Error! There is more than one GameManager in the scene!");
+        else
+            Instance = this;
+    }
+
+    public void SetGameState(int state) {
+        SetGameState((GameState) state);
+    }
+    public void SetGameState(GameState state) {
+        if (state == GameState.Running) {
+            startMenu.SetActive(false);
+            PlayerController.Instance.playerAnimator.SetTrigger("switchToRun");
+        }
+        if (state == GameState.KickingBall) {
+            PlayerController.Instance.playerAnimator.SetTrigger("switchToKick");
+        }
+        if (state == GameState.BallKicked) {
+            PlayerController.Instance.KickBall();
+        }
+
+        GameManager.state = state;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController Instance;
+
+    const float playerSpeed = 3;
+
+    [SerializeField] Renderer playerRenderer;
+    [SerializeField] Renderer ballRenderer;
+    [SerializeField] public Animator playerAnimator;
+    [SerializeField] Material greenMaterial;
+    [SerializeField] Material yellowMaterial;
+    [SerializeField] Material redMaterial;
+    [SerializeField] GameObject ball;
+
+
+    ChangeColorWall.WallColor playerColor = ChangeColorWall.WallColor.Green;
+    bool isMousedDown = false;
+    Vector3 mousedDownLocation;
+    float origXLocation;
+    float baseXLocation;
+    const float movementRangeX = 1;
+
+    void Awake() {
+        if (Instance != null)
+            Debug.LogError("Error! There is more than one PlayerController in the scene!");
+        else
+            Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseXLocation = transform.position.x;
+    }
+
+    public void KickBall() {
+        ball.GetComponent<Rigidbody>().useGravity = true;
+        ball.GetComponent<Rigidbody>().isKinematic = false;
+        ball.GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.Continuous;
+        ball.GetComponent<Rigidbody>().AddForce(ball.transform.forward * 1000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.state == GameManager.GameState.Running) {
+
+        
+
+            if (Input.GetMouseButtonDown(0) && isMousedDown == false) {
+            isMousedDown = true;
+            mousedDownLocation = Input.mousePosition;
+            origXLocation = transform.position.x;
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            isMousedDown = false;
+            mousedDownLocation = Vector3.zero;
+        }
+
+        if (isMousedDown) {
+            float rawMove = mousedDownLocation.x - Input.mousePosition.x;
+            float percentMove = rawMove / Screen.width;
+            Vector3 currentLocation = transform.position;
+            currentLocation.x = origXLocation - (percentMove * 3);
+            if (currentLocation.x > (baseXLocation+ movementRangeX)) {
+                currentLocation.x = (baseXLocation + movementRangeX);
+            }
+            else if (currentLocation.x < (baseXLocation - movementRangeX)) {
+                currentLocation.x = (baseXLocation - movementRangeX);
+            }
+            transform.position = currentLocation;
+        }
+
+            transform.position = transform.position + ((transform.forward * playerSpeed) * Time.deltaTime);
+        }
+        else if (GameManager.state == GameManager.GameState.KickingBall) {
+            ball.transform.position = ball.transform.position + ((ball.transform.forward * playerSpeed/10) * Time.deltaTime);
+        }
+
+    }
+
+    void SetPlayerColor(ChangeColorWall.WallColor color) {
+        Material materialToAssign = null;
+
+        switch (color) {
+            case ChangeColorWall.WallColor.Green:
+                materialToAssign = greenMaterial;
+                break;
+            case ChangeColorWall.WallColor.Yellow:
+                materialToAssign = yellowMaterial;
+                break;
+            case ChangeColorWall.WallColor.Red:
+                materialToAssign = redMaterial;
+                break;
+        }
+
+        Material[] currentMaterials = playerRenderer.materials;
+        currentMaterials[2] = materialToAssign;
+        playerRenderer.materials = currentMaterials;
+        currentMaterials = ballRenderer.materials;
+        currentMaterials[0] = materialToAssign;
+        ballRenderer.materials = currentMaterials;
+
+        playerColor = color;
+    }
+
+    void PickUpBall(ChangeColorWall.WallColor color) {
+        if (playerColor == color) {
+            Debug.Log("ball increased");
+        }
+        else {
+            Debug.Log("ball decreased");
+        }
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        switch (other.tag) {
+            case "Change Color Wall":
+                SetPlayerColor(other.GetComponent<ChangeColorWall>().color);
+                break;
+            case "Pickup Ball":
+                PickUpBall(other.GetComponent<PickupBall>().color);
+                break;
+            case "Goal Post":
+                GameManager.Instance.SetGameState(GameManager.GameState.KickingBall);
+                break;
+        }
+    }
+}
