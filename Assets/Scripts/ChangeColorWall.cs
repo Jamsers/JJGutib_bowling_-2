6981c328b466bb38efbdc35c4055c985a98b78ca
John James Gutib <jjgutib@gmail.com>
2021-05-24 15:20:01 +0800

    Implemented pickups and color changing wall

---
+++ Assets/Scripts/ChangeColorWall.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class ChangeColorWall : MonoBehaviour {
+    [SerializeField] WallColor wallColor;
+
+    [SerializeField] Material greenMaterial;
+    [SerializeField] Material yellowMaterial;
+    [SerializeField] Material redMaterial;
+
+    [System.Serializable]
+    public enum WallColor {
+        Green,
+        Yellow,
+        Red
+    }
+
+    public WallColor color {
+        get => wallColor;
+    }
+
+    void Start() {
+        Material materialToAssign = null;
+
+        switch (wallColor) {
+            case WallColor.Green:
+                materialToAssign = greenMaterial;
+                break;
+            case WallColor.Yellow:
+                materialToAssign = yellowMaterial;
+                break;
+            case WallColor.Red:
+                materialToAssign = redMaterial;
+                break;
+        }
+
+        for (int i = 0; i < transform.childCount; i++) {
+            Renderer renderer = transform.GetChild(i).GetComponent<Renderer>();
+            Material[] currentMaterials = renderer.materials;
+            currentMaterials[1] = materialToAssign;
+            renderer.materials = currentMaterials;
+        }
+    }
+}
---
+++ Assets/Scripts/PickupBall.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PickupBall : MonoBehaviour {
+    [SerializeField] ChangeColorWall.WallColor ballColor;
+
+    [SerializeField] Material greenMaterial;
+    [SerializeField] Material yellowMaterial;
+    [SerializeField] Material redMaterial;
+
+    public ChangeColorWall.WallColor color {
+        get => ballColor;
+    }
+
+    void Start() {
+        Material materialToAssign = null;
+
+        switch (ballColor) {
+            case ChangeColorWall.WallColor.Green:
+                materialToAssign = greenMaterial;
+                break;
+            case ChangeColorWall.WallColor.Yellow:
+                materialToAssign = yellowMaterial;
+                break;
+            case ChangeColorWall.WallColor.Red:
+                materialToAssign = redMaterial;
+                break;
+        }
+
+        Renderer renderer = transform.GetChild(0).GetComponent<Renderer>();
+        Material[] currentMaterials = renderer.materials;
+        currentMaterials[0] = materialToAssign;
+        renderer.materials = currentMaterials;
+    }
+}
